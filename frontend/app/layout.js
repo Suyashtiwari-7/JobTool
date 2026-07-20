3efc22300634d83cd8fe8f2ab498774348c6d83f
Suyash Jagdish Tiwari <suyashtiwari7272@gmail.com>
2026-07-21 04:29:30 +0530

    UI tweak: Hardcode HTML head link tag to force favicon update

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" type="image/png" />
+      </head>
       <body>{children}</body>
     </html>
   );
